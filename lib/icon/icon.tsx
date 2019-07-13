e238b8b1fe1076ffaab5b358fda4292c137d2708
starkxj <starkxj@163.com>
2019-07-13 23:50:13 +0800

    删除注释

--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -6,7 +6,7 @@ import classes from '../helpers/classes';
 interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string;
 }
-// 测试添加注释
+
 const Icon: React.FunctionComponent<IconProps> =
   ({className, name, ...restProps}) => {
     return (
