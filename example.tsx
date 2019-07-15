4cc4cccae716214f761d97ff5eb105e0cd80ea02
starkxj <starkxj@163.com>
2019-07-16 00:10:30 +0800

    add layout组件

--- example.tsx
+++ example.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter as Router, Route, Link} from 'react-router-dom'
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import IconExample from './lib/icon/icon.example';
+import DialogExample from './lib/dialog/dialog.example';
 import ButtonExample from './lib/button.example';
+import LayoutExample from './lib/layout/layout.example';
+
 
 ReactDOM.render(
   <Router>
@@ -23,11 +26,19 @@ ReactDOM.render(
             <li>
               <Link to="/button">Button</Link>
             </li>
+            <li>
+              <Link to="/dialog">对话框</Link>
+            </li>
+            <li>
+              <Link to="/layout">布局</Link>
+            </li>
           </ul>
         </aside>
         <main>
           <Route path="/icon" component={IconExample}/>
           <Route path="/button" component={ButtonExample}/>
+          <Route path="/dialog" component={DialogExample}/>
+          <Route path="/layout" component={LayoutExample}/>
         </main>
       </div>
     </div>
